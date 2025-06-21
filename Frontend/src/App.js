a48763a7b5b6055767a9701bde992dbb98879942
Sahil Sahu <166105124+Sahilsahu223@users.noreply.github.com>
2025-06-21 13:44:44 +0530

    Add files via upload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,25 @@
-
-import './App.css';
-import Appbar from './components/Appbar'
-import Student from './components/Student'
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddStudent from "./students/AddUser";
+import EditStudent from "./students/EditUser";
+import ViewStudent from "./students/ViewUser";
 
 function App() {
   return (
     <div className="App">
-    <Appbar/>
-    <Student/>
-   
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddStudent />} />
+          <Route exact path="/edituser/:id" element={<EditStudent />} />
+          <Route exact path="/viewuser/:id" element={<ViewStudent />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
