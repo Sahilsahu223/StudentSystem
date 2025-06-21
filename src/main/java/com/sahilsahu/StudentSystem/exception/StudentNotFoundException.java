1917ef7e8884acc5a4baed700ca15bd92bc953c0
Sahil Sahu <166105124+Sahilsahu223@users.noreply.github.com>
2025-06-21 12:59:54 +0530

    Delete src directory

--- src/main/java/com/sahilsahu/StudentSystem/exception/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sahilsahu.StudentSystem.exception;
-
-public class StudentNotFoundException extends RuntimeException{
-    public StudentNotFoundException(Long id){
-        super("Could not found the user with id "+ id);
-    }
-}
